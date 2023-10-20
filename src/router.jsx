342ffbc26f3976867c6673400ac09c9b1e356b9b
Aniket <aniketdevmore1008@gmail.com>
2023-10-20 10:46:39 +0530

    add lazy loading

--- src/router.jsx
+++ src/router.jsx
@@ -1,29 +1,60 @@
+import React, { lazy, Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
-import ProductInfo from "./components/productInfo/ProductInfo";
-import AllProducts from "./components/allProducts/AllProducts";
-import Gallery from "./components/gallery/Gallery";
-import Contact from "./components/contact/Contact";
+const App = React.lazy(() => import("./App"));
+// import App from "./App";
+const ProductInfo = React.lazy(() =>
+  import("./components/productInfo/ProductInfo")
+);
+// import ProductInfo from "./components/productInfo/ProductInfo";
+const AllProducts = React.lazy(() =>
+  import("./components/allProducts/AllProducts")
+);
+// import AllProducts from "./components/allProducts/AllProducts";
+const Gallery = React.lazy(() => import("./components/gallery/Gallery"));
+// import Gallery from "./components/gallery/Gallery";
+const Contact = React.lazy(() => import("./components/contact/Contact"));
+// import Contact from "./components/contact/Contact";
+import Loading from "./components/loading/Loading";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: (
+      <Suspense fallback={<Loading />}>
+        <App />
+      </Suspense>
+    ),
   },
   {
     path: "/features/:id",
-    element: <ProductInfo />,
+    element: (
+      <Suspense fallback={<Loading />}>
+        <ProductInfo />
+      </Suspense>
+    ),
   },
   {
     path: "/products",
-    element: <AllProducts />,
+    element: (
+      <Suspense fallback={<Loading />}>
+        <AllProducts />
+      </Suspense>
+    ),
   },
   {
     path: "/gallery",
-    element: <Gallery />,
+    element: (
+      <Suspense fallback={<Loading />}>
+        <Gallery />
+      </Suspense>
+    ),
   },
   {
     path: "/contact-us",
-    element: <Contact />,
+    element: (
+      <Suspense fallback={<Loading />}>
+        <Contact />
+      </Suspense>
+    ),
   },
 ]);
 
